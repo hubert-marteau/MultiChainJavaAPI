a0b5848f88a12f9ee9c99c212804b379e5648ee5
Hubert MARTEAU <hubertmarteau@hotmail.com>
2017-07-24 12:22:41 +0200

    Add files via upload

---
+++ src/multichain/object/SignedTransactionRAW.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2017 Worldline, Inc.
+ *
+ * MultiChainJavaAPI code distributed under the GPLv3 license, see COPYING file.
+ * https://github.com/SimplyUb/MultiChainJavaAPI/blob/master/LICENSE
+ *
+ */
+package multichain.object;
+
+/**
+ * @author Ub - H. MARTEAU
+ * @version 1.1
+ */
+public class SignedTransactionRAW {
+	String hex;
+	boolean complete;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "SignedTransactionRAW [hex=" + hex + ", complete=" + complete + "]";
+	}
+
+	/**
+	 * @return the hex
+	 */
+	public String getHex() {
+		return hex;
+	}
+
+	/**
+	 * @param hex
+	 *            the hex to set
+	 */
+	public void setHex(String hex) {
+		this.hex = hex;
+	}
+
+	/**
+	 * @return the complete
+	 */
+	public boolean isComplete() {
+		return complete;
+	}
+
+	/**
+	 * @param complete
+	 *            the complete to set
+	 */
+	public void setComplete(boolean complete) {
+		this.complete = complete;
+	}
+
+}
