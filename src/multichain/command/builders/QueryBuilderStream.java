1da38f949ce3d7195af8efea7e1b1ab7a21325ca
Chloé Moreau <chloe.moreau37@hotmail.fr>
2017-09-12 16:43:47 +0200

    Add MessagingCommand (signmessage + verifymessage); Add createfrom

--- src/multichain/command/builders/QueryBuilderStream.java
+++ src/multichain/command/builders/QueryBuilderStream.java
@@ -21,12 +21,12 @@ public class QueryBuilderStream extends QueryBuilderCommon {
 	 * Creates stream
 	 * 
 	 * 
-	 * Arguments: 1. entity-type (string, required) The only possible value:
-	 * stream 2. "stream-name" (string, required) Stream name, if not "" should
-	 * be unique. 3. open (boolean, required) Allow anyone to publish in this
-	 * stream 4 custom-fields (object, optional) a json object with custom
-	 * fields { "param-name": "param-value" (strings, required) The key is the
-	 * parameter name, the value is parameter value ,... }
+	 * Arguments: 1. entity-type (string, required) The only possible value: stream
+	 * 2. "stream-name" (string, required) Stream name, if not "" should be unique.
+	 * 3. open (boolean, required) Allow anyone to publish in this stream 4
+	 * custom-fields (object, optional) a json object with custom fields {
+	 * "param-name": "param-value" (strings, required) The key is the parameter
+	 * name, the value is parameter value ,... }
 	 * 
 	 * Result: "transactionid" (string) The transaction id.
 	 * 
@@ -42,25 +42,44 @@ public class QueryBuilderStream extends QueryBuilderCommon {
 	}
 
 	/**
+	 * {@link #executeCreate(String, boolean)} with control over the from-address
+	 * used to create the stream
 	 * 
-	 * liststreamkeys "stream-identifier" ( key(s) verbose count start local-ordering )
+	 * @param addressFrom
+	 *            (String) The from-address used to create the stream
+	 * @param streamName
+	 *            (String) The name of the stream to create
+	 * @param open
+	 *            (Boolean) True if the stream is open, else false
+	 * @return (String) The transaction id
+	 * @throws MultichainException
+	 */
+	protected Object executeCreateFrom(String addressFrom, String streamName, boolean open) throws MultichainException {
+		MultichainTestParameter.isNotNullOrEmpty("streamName", streamName);
+		MultichainTestParameter.isNotNullOrEmpty("addressFrom", addressFrom);
+
+		return execute(CommandEnum.CREATEFROM, addressFrom, "stream", streamName, open);
+	}
+
+	/**
+	 * 
+	 * liststreamkeys "stream-identifier" ( key(s) verbose count start
+	 * local-ordering )
 	 * 
 	 * Returns stream keys.
 	 * 
-	 * Arguments:
-	 * 1. "stream-identifier"(string, required) Stream identifier - one of the following: stream txid, stream reference,
-	 * stream name.
-	 * 2. "key" (string, optional, default=*) Stream key
-	 * or
-	 * 2. key(s) (array, optional) A json array of stream keys
-	 * 3. verbose (boolean, optional, default=false) If true, returns extended information about key
-	 * 4. count (number, optional, default=INT_MAX - all) The number of items to display
-	 * 5. start (number, optional, default=-count - last) Start from specific item, 0 based, if negative - from the end
-	 * 6. local-ordering (boolean, optional, default=false) If true, items appear in the order they were processed by
-	 * the wallet, if false - in the order they apppear in blockchain
+	 * Arguments: 1. "stream-identifier"(string, required) Stream identifier - one
+	 * of the following: stream txid, stream reference, stream name. 2. "key"
+	 * (string, optional, default=*) Stream key or 2. key(s) (array, optional) A
+	 * json array of stream keys 3. verbose (boolean, optional, default=false) If
+	 * true, returns extended information about key 4. count (number, optional,
+	 * default=INT_MAX - all) The number of items to display 5. start (number,
+	 * optional, default=-count - last) Start from specific item, 0 based, if
+	 * negative - from the end 6. local-ordering (boolean, optional, default=false)
+	 * If true, items appear in the order they were processed by the wallet, if
+	 * false - in the order they apppear in blockchain
 	 * 
-	 * Result:
-	 * "stream-keys" (array) List of stream keys.
+	 * Result: "stream-keys" (array) List of stream keys.
 	 * 
 	 * @param streamName
 	 * @param key
@@ -98,24 +117,23 @@ public class QueryBuilderStream extends QueryBuilderCommon {
 
 	/**
 	 * 
-	 * liststreamkeys "stream-identifier" ( key(s) verbose count start local-ordering )
+	 * liststreamkeys "stream-identifier" ( key(s) verbose count start
+	 * local-ordering )
 	 * 
 	 * Returns stream keys.
 	 * 
-	 * Arguments:
-	 * 1. "stream-identifier"(string, required) Stream identifier - one of the following: stream txid, stream reference,
-	 * stream name.
-	 * 2. "key" (string, optional, default=*) Stream key
-	 * or
-	 * 2. key(s) (array, optional) A json array of stream keys
-	 * 3. verbose (boolean, optional, default=false) If true, returns extended information about key
-	 * 4. count (number, optional, default=INT_MAX - all) The number of items to display
-	 * 5. start (number, optional, default=-count - last) Start from specific item, 0 based, if negative - from the end
-	 * 6. local-ordering (boolean, optional, default=false) If true, items appear in the order they were processed by
-	 * the wallet, if false - in the order they apppear in blockchain
+	 * Arguments: 1. "stream-identifier"(string, required) Stream identifier - one
+	 * of the following: stream txid, stream reference, stream name. 2. "key"
+	 * (string, optional, default=*) Stream key or 2. key(s) (array, optional) A
+	 * json array of stream keys 3. verbose (boolean, optional, default=false) If
+	 * true, returns extended information about key 4. count (number, optional,
+	 * default=INT_MAX - all) The number of items to display 5. start (number,
+	 * optional, default=-count - last) Start from specific item, 0 based, if
+	 * negative - from the end 6. local-ordering (boolean, optional, default=false)
+	 * If true, items appear in the order they were processed by the wallet, if
+	 * false - in the order they apppear in blockchain
 	 * 
-	 * Result:
-	 * "stream-keys" (array) List of stream keys.
+	 * Result: "stream-keys" (array) List of stream keys.
 	 * 
 	 * @param streamName
 	 * @param key
@@ -136,7 +154,8 @@ public class QueryBuilderStream extends QueryBuilderCommon {
 
 	/**
 	 * 
-	 * {@link executeListStreamKeys(String streamName, String key, boolean verbose, int count, int start)} without start
+	 * {@link executeListStreamKeys(String streamName, String key, boolean verbose,
+	 * int count, int start)} without start
 	 * 
 	 * @param streamName
 	 * @param key
@@ -160,15 +179,15 @@ public class QueryBuilderStream extends QueryBuilderCommon {
 	 * 
 	 * Returns stream items for specific key.
 	 * 
-	 * Arguments: 1. "stream-identifier"(string, required) Stream identifier -
-	 * one of the following: stream txid, stream reference, stream name. 2.
-	 * "key" (string, required) Stream key 3. verbose (boolean, optional,
-	 * default=false) If true, returns information about item transaction 4.
-	 * count (number, optional, default=10) The number of items to display 5.
-	 * start (number, optional, default=-count - last) Start from specific item,
-	 * 0 based, if negative - from the end 6. local-ordering (boolean, optional,
-	 * default=false) If true, items appear in the order they were processed by
-	 * the wallet, if false - in the order they appear in blockchain
+	 * Arguments: 1. "stream-identifier"(string, required) Stream identifier - one
+	 * of the following: stream txid, stream reference, stream name. 2. "key"
+	 * (string, required) Stream key 3. verbose (boolean, optional, default=false)
+	 * If true, returns information about item transaction 4. count (number,
+	 * optional, default=10) The number of items to display 5. start (number,
+	 * optional, default=-count - last) Start from specific item, 0 based, if
+	 * negative - from the end 6. local-ordering (boolean, optional, default=false)
+	 * If true, items appear in the order they were processed by the wallet, if
+	 * false - in the order they appear in blockchain
 	 * 
 	 * Result: "stream-items" (array) List of stream items for specific key.
 	 * 
@@ -195,15 +214,15 @@ public class QueryBuilderStream extends QueryBuilderCommon {
 	 * 
 	 * Returns stream items for specific key.
 	 * 
-	 * Arguments: 1. "stream-identifier"(string, required) Stream identifier -
-	 * one of the following: stream txid, stream reference, stream name. 2.
-	 * "key" (string, required) Stream key 3. verbose (boolean, optional,
-	 * default=false) If true, returns information about item transaction 4.
-	 * count (number, optional, default=10) The number of items to display 5.
-	 * start (number, optional, default=-count - last) Start from specific item,
-	 * 0 based, if negative - from the end 6. local-ordering (boolean, optional,
-	 * default=false) If true, items appear in the order they were processed by
-	 * the wallet, if false - in the order they appear in blockchain
+	 * Arguments: 1. "stream-identifier"(string, required) Stream identifier - one
+	 * of the following: stream txid, stream reference, stream name. 2. "key"
+	 * (string, required) Stream key 3. verbose (boolean, optional, default=false)
+	 * If true, returns information about item transaction 4. count (number,
+	 * optional, default=10) The number of items to display 5. start (number,
+	 * optional, default=-count - last) Start from specific item, 0 based, if
+	 * negative - from the end 6. local-ordering (boolean, optional, default=false)
+	 * If true, items appear in the order they were processed by the wallet, if
+	 * false - in the order they appear in blockchain
 	 * 
 	 * Result: "stream-items" (array) List of stream items for specific key.
 	 * 
@@ -230,10 +249,10 @@ public class QueryBuilderStream extends QueryBuilderCommon {
 	 * Publishes stream item
 	 * 
 	 * 
-	 * Arguments: 1. "stream-identifier" (string, required) Stream identifier -
-	 * one of the following: stream txid, stream reference, stream name. 2.
-	 * "key" (string, required) Item key 3. data-hex (string, required) Item
-	 * data hex string
+	 * Arguments: 1. "stream-identifier" (string, required) Stream identifier - one
+	 * of the following: stream txid, stream reference, stream name. 2. "key"
+	 * (string, required) Item key 3. data-hex (string, required) Item data hex
+	 * string
 	 * 
 	 * Result: "transactionid" (string) The transaction id.
 	 * 
@@ -257,15 +276,13 @@ public class QueryBuilderStream extends QueryBuilderCommon {
 	 * 
 	 * Subscribes to the stream.
 	 * 
-	 * Arguments:
-	 * 1. "stream-identifier" (string, required) Stream identifier - one of the following: stream txid, stream
-	 * reference, stream name.
-	 * or
-	 * 1. "asset-identifier" (string, required) Asset identifier - one of the following: asset txid, asset reference,
-	 * asset name.
-	 * or
-	 * 1. entity-identifier(s) (array, optional) A json array of stream or asset identifiers
-	 * 2. rescan (boolean, optional, default=true) Rescan the wallet for transactions
+	 * Arguments: 1. "stream-identifier" (string, required) Stream identifier - one
+	 * of the following: stream txid, stream reference, stream name. or 1.
+	 * "asset-identifier" (string, required) Asset identifier - one of the
+	 * following: asset txid, asset reference, asset name. or 1.
+	 * entity-identifier(s) (array, optional) A json array of stream or asset
+	 * identifiers 2. rescan (boolean, optional, default=true) Rescan the wallet for
+	 * transactions
 	 * 
 	 * Note: This call can take minutes to complete if rescan is true.
 	 * 
@@ -283,14 +300,12 @@ public class QueryBuilderStream extends QueryBuilderCommon {
 	 * 
 	 * Unsubscribes from the stream.
 	 * 
-	 * Arguments:
-	 * 1. "stream-identifier" (string, required) Stream identifier - one of the following: stream txid, stream
-	 * reference, stream name.
-	 * or
-	 * 1. "asset-identifier" (string, required) Asset identifier - one of the following: asset txid, asset reference,
-	 * asset name.
-	 * or
-	 * 1. entity-identifier(s) (array, optional) A json array of stream or asset identifiers
+	 * Arguments: 1. "stream-identifier" (string, required) Stream identifier - one
+	 * of the following: stream txid, stream reference, stream name. or 1.
+	 * "asset-identifier" (string, required) Asset identifier - one of the
+	 * following: asset txid, asset reference, asset name. or 1.
+	 * entity-identifier(s) (array, optional) A json array of stream or asset
+	 * identifiers
 	 * 
 	 * @param streamName
 	 * @throws MultichainException
