8623883e2c0bd7950fc8143f2a06241a4609a539
[a561842] Hubert Marteau <hubert.marteau@atos.net>
2017-09-13 13:47:04 +0200

    First step to JUnit tests

--- src/test/java/multichain/command/AddressCommandTest.java
+++ src/test/java/multichain/command/AddressCommandTest.java
@@ -1,33 +1,19 @@
-/*
- * Copyright (C) 2017 Worldline, Inc.
- *
- * MultiChainJavaAPI code distributed under the GPLv3 license, see COPYING file.
- * https://github.com/SimplyUb/MultiChainJavaAPI/blob/master/LICENSE
- *
- */
 package multichain.command;
 
 import java.util.List;
 
-import multichain.command.MultiChainCommand;
-import multichain.command.MultichainException;
+import org.junit.Test;
+
+import junit.framework.TestCase;
 import multichain.object.Address;
 
-/**
- * @author Ub - H. MARTEAU
- * @version 3.2
- */
-public class AddressCommandTest {
-	static MultiChainCommand multiChainCommand;
+public class AddressCommandTest extends TestCase {
 
-	/**
-	 *
-	 */
-	public AddressCommandTest() {
-		// TODO Auto-generated constructor stub
-	}
+	@Test
+	public void testgetAddresses() {
+		MultiChainCommand multiChainCommand = new MultiChainCommand(TestConst.MULTICHAIN_SERVER_IP,
+				TestConst.MULTICHAIN_SERVER_PORT, TestConst.MULTICHAIN_SERVER_LOGIN, TestConst.MULTICHAIN_SERVER_PWD);
 
-	private static void testgetAddresses() {
 		List<String> result = null;
 		List<Address> resultAddress = null;
 		try {
@@ -36,52 +22,40 @@ public class AddressCommandTest {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		if (result == null || result.size() == 0) {
-			System.err.println("testgetAddresses - result list is empty");
-		} else {
-			System.out.println("testgetAddresses - Result :");
-			for (String ad : result) {
-				System.out.println(ad);
-			}
-		}
-		System.out.println("");
-		System.out.println("");
+
+		assertNotNull(result);
+		assertFalse(result.size() == 0);
+
 		try {
 			resultAddress = multiChainCommand.getAddressCommand().getAddressesList();
 		} catch (MultichainException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		if (resultAddress == null || resultAddress.size() == 0) {
-			System.err.println("testgetAddressesList - result list is empty");
-		} else {
-			System.out.println("testgetAddressesList - Result :");
-			for (Address ad : resultAddress) {
-				System.out.println(ad.toString());
-			}
-		}
-		System.out.println("");
-		System.out.println("");
+
+		assertNotNull(resultAddress);
+		assertFalse(resultAddress.size() == 0);
 	}
 
-	private static void testvalidateAddress() {
+	@Test
+	public void testvalidateAddress() {
+		MultiChainCommand multiChainCommand = new MultiChainCommand(TestConst.MULTICHAIN_SERVER_IP,
+				TestConst.MULTICHAIN_SERVER_PORT, TestConst.MULTICHAIN_SERVER_LOGIN, TestConst.MULTICHAIN_SERVER_PWD);
+
 		List<String> resultAddresses = null;
 		Address result = null;
 		try {
 			resultAddresses = multiChainCommand.getAddressCommand().getAddresses();
-			if (resultAddresses != null && resultAddresses.size() != 0) {
-				for (String addressString : resultAddresses) {
-					result = multiChainCommand.getAddressCommand().validateAddress(addressString);
-					System.out.println("testvalidateAddress - result for address " + addressString + " :");
-					System.out.println(result);
 
-					if (result == null || result.getAddress() == null || "".equals(result.getAddress())) {
-						System.err.println("testvalidateAddress - result is not correct");
-					}
-					System.out.println("");
-				}
-			} else {
-				System.err.println("testgetAddresses - result list is empty");
+			assertNotNull(resultAddresses);
+			assertFalse(resultAddresses.size() == 0);
+
+			for (String addressString : resultAddresses) {
+				result = multiChainCommand.getAddressCommand().validateAddress(addressString);
+
+				assertNotNull(result);
+				assertNotNull(result.getAddress());
+				assertNotSame("", result.getAddress());
 			}
 		} catch (MultichainException e) {
 			// TODO Auto-generated catch block
@@ -89,57 +63,30 @@ public class AddressCommandTest {
 		}
 	}
 
-	private static void testgetNewAddress() {
+	@Test
+	public void testgetNewAddress() {
+		MultiChainCommand multiChainCommand = new MultiChainCommand(TestConst.MULTICHAIN_SERVER_IP,
+				TestConst.MULTICHAIN_SERVER_PORT, TestConst.MULTICHAIN_SERVER_LOGIN, TestConst.MULTICHAIN_SERVER_PWD);
+
 		String resultAddress = "";
 		Address result = null;
 
-		System.out.println("testgetNewAddress");
 		try {
 			resultAddress = multiChainCommand.getAddressCommand().getNewAddress();
-			if (resultAddress != null && !"".equals(resultAddress)) {
-				System.out.println("   getNewAddress :");
-				System.out.println(resultAddress);
-			} else {
-				System.err.println("getNewAddress - result is empty");
-			}
-			System.out.println();
-			System.out.println();
+
+			assertNotNull(resultAddress);
+			assertNotSame("", resultAddress);
 
 			result = multiChainCommand.getAddressCommand().getNewAddressFilled();
-			if (result != null && result.getAddress() != null && !"".equals(result.getAddress())) {
-				System.out.println("   getNewAddressFilled :");
-				System.out.println(result);
-			} else {
-				System.err.println("getNewAddress - result is empty");
-			}
-			System.out.println();
-			System.out.println();
+
+			assertNotNull(result);
+			assertNotNull(result.getAddress());
+			assertNotSame("", result.getAddress());
 
 		} catch (MultichainException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		System.out.println("--- Start of AddressCommandTest ---");
-
-		// BlockChain TestCommand has to be created and started before
-		multiChainCommand = new MultiChainCommand("localhost", "6824", "multichainrpc",
-				"73oYQWzx45hossFPPWUgicpLvHhsD8PempYxnSF6bnY9");
-
-		testgetAddresses();
-		testvalidateAddress();
-
-		System.out.println();
-		System.out.println();
-
-		testgetNewAddress();
-
-		System.out.println("--- End of AddressCommandTest ---");
 	}
 
 }
---
+++ src/test/java/multichain/command/TestConst.java
@@ -0,0 +1,8 @@
+package multichain.command;
+
+public class TestConst {
+	public static String MULTICHAIN_SERVER_IP = "localhost";
+	public static String MULTICHAIN_SERVER_PORT = "6824";
+	public static String MULTICHAIN_SERVER_LOGIN = "multichainrpc";
+	public static String MULTICHAIN_SERVER_PWD = "73oYQWzx45hossFPPWUgicpLvHhsD8PempYxnSF6bnY9";
+}
