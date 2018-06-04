3fd2684162d9dfeffb087fd0725106294f01abc0
ub <hubert.marteau@worldline.com>
2018-06-04 08:13:42 +0200

    simple clean install

--- install.sh
+++ install.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-mvn compile
-mvn package
-mvn install:install-file -Dfile=target/MultiChainJavaAPI-0.4.11-SNAPSHOT.jar -DgroupId=com.multichainjavaapi -DartifactId=MultiChainJavaAPI -Dversion=0.4.11
-mvn install:install-file -Dfile=target/MultiChainJavaAPI-0.4.11-SNAPSHOT-jar-with-dependencies.jar -DgroupId=com.multichainjavaapi -DartifactId=MultiChainJavaAPI -Dversion=0.4.11-full
+mvn clean install
