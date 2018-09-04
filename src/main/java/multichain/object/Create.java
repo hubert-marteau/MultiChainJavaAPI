7268a865ad1cd7929a38fe5505dcf520aa7a0c93
[a561842] hubert marteau <hubert.marteau@atos.net>
2018-09-04 14:11:54 +0200

    Creation Object

---
+++ src/main/java/multichain/object/Create.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2017 Worldline, Inc.
+ *
+ * MultiChainJavaAPI code distributed under the GPLv3 license, see COPYING file.
+ * https://github.com/SimplyUb/MultiChainJavaAPI/blob/master/LICENSE
+ *
+ */
+package multichain.object;
+
+/**
+ * @author Ub - H. MARTEAU
+ * @version 4.14
+ */
+public class Create extends Stream {
+	String type = null;
+
+	/**
+	 * @return the type
+	 */
+	public String getType() {
+		return type;
+	}
+
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	
+}
