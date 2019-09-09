fd47b9001a225bba1db8370c21cba8f848f7842d
[a561842] hubert marteau <hubert.marteau@atos.net>
2019-09-09 16:15:38 +0200

    Version 2.0.1 - Init

---
+++ src/test/java/multichain/command/CommandManagerTest.java
@@ -0,0 +1,196 @@
+/*
+ * Copyright (C) 2017 Worldline, Inc.
+ *
+ * MultiChainJavaAPI code distributed under the GPLv3 license, see COPYING file.
+ * https://github.com/SimplyUb/MultiChainJavaAPI/blob/master/LICENSE
+ *
+ */
+package multichain.command;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import org.junit.Test;
+import junit.framework.TestCase;
+
+/**
+ * @author Ub - H. MARTEAU
+ * @version 2.0.1
+ */
+public class CommandManagerTest extends TestCase {
+
+
+  @Test
+  public void testPingPauseResume() {
+    System.out.println("--- testGetInfo ---");
+
+    CommandManager commandManager =
+        new CommandManager(TestConst.MULTICHAIN_SERVER_IP, TestConst.MULTICHAIN_SERVER_PORT,
+            TestConst.MULTICHAIN_SERVER_LOGIN, TestConst.MULTICHAIN_SERVER_PWD, null);
+
+    Object result = null;
+
+    try {
+      result = commandManager.invoke(CommandElt.PING);
+    } catch (MultichainException e) {
+      e.printStackTrace();
+      fail();
+    }
+
+    assertNull(result);
+
+    try {
+      result = commandManager.invoke(CommandElt.PAUSE, "offchain");
+    } catch (MultichainException e) {
+      e.printStackTrace();
+      fail();
+    }
+
+    assertNotNull(result);
+    assertEquals("Paused", result);
+
+    try {
+      result = commandManager.invoke(CommandElt.RESUME, "offchain");
+    } catch (MultichainException e) {
+      e.printStackTrace();
+      fail();
+    }
+
+    assertNotNull(result);
+    assertEquals("Resumed", result);
+
+  }
+
+  @Test
+  public void testGetInfo() {
+    System.out.println("--- testGetInfo ---");
+
+    CommandManager commandManager =
+        new CommandManager(TestConst.MULTICHAIN_SERVER_IP, TestConst.MULTICHAIN_SERVER_PORT,
+            TestConst.MULTICHAIN_SERVER_LOGIN, TestConst.MULTICHAIN_SERVER_PWD, null);
+
+    Object result = null;
+
+    try {
+      result = commandManager.invoke(CommandElt.GETINFO);
+    } catch (MultichainException e) {
+      e.printStackTrace();
+      fail();
+    }
+
+    assertNotNull(result);
+
+    System.out.println(result);
+    System.out.println(result.getClass());
+  }
+
+  @Test
+  public void testGetAddresses() {
+    System.out.println("--- testGetAddresses ---");
+
+    CommandManager commandManager =
+        new CommandManager(TestConst.MULTICHAIN_SERVER_IP, TestConst.MULTICHAIN_SERVER_PORT,
+            TestConst.MULTICHAIN_SERVER_LOGIN, TestConst.MULTICHAIN_SERVER_PWD, null);
+
+    Object addressResult = null;
+
+    try {
+      addressResult = commandManager.invoke(CommandElt.GETADDRESSES);
+    } catch (MultichainException e) {
+      e.printStackTrace();
+      fail();
+    }
+
+    assertNotNull(addressResult);
+
+    System.out.println(addressResult);
+    System.out.println(addressResult.getClass());
+    if (ArrayList.class.isInstance(addressResult) && addressResult != null
+        && ((ArrayList<?>) addressResult).size() > 0) {
+      System.out.println(((ArrayList<?>) addressResult).get(0).getClass());
+    }
+
+
+    try {
+      addressResult = commandManager.invoke(CommandElt.GETADDRESSES, true);
+    } catch (MultichainException e) {
+      e.printStackTrace();
+      fail();
+    }
+
+    assertNotNull(addressResult);
+
+    System.out.println(addressResult);
+    System.out.println(addressResult.getClass());
+    if (ArrayList.class.isInstance(addressResult) && addressResult != null
+        && ((ArrayList<?>) addressResult).size() > 0) {
+      System.out.println(((ArrayList<?>) addressResult).get(0).getClass());
+    }
+
+
+  }
+
+  @Test
+  public void testAddMultisigAddress() {
+    System.out.println("--- testAddMultisigAddress ---");
+
+    CommandManager commandManager =
+        new CommandManager(TestConst.MULTICHAIN_SERVER_IP, TestConst.MULTICHAIN_SERVER_PORT,
+            TestConst.MULTICHAIN_SERVER_LOGIN, TestConst.MULTICHAIN_SERVER_PWD, null);
+
+    Object result = null;
+
+    try {
+      result = commandManager.invoke(CommandElt.GETADDRESSES);
+    } catch (MultichainException e) {
+      e.printStackTrace();
+      fail();
+    }
+
+    assertNotNull(result);
+
+    String[] addresses = null;
+
+    if (ArrayList.class.isInstance(result) && result != null
+        && ((ArrayList<?>) result).size() > 1) {
+      addresses =
+          new String[] {((ArrayList<String>) result).get(0), ((ArrayList<String>) result).get(1)};
+    }
+
+    try {
+      result = commandManager.invoke(CommandElt.ADDMULTISIGADDRESS, 1, addresses);
+    } catch (MultichainException e) {
+      e.printStackTrace();
+      fail();
+    }
+
+    assertNotNull(result);
+
+    System.out.println(result);
+    System.out.println(result.getClass());
+  }
+
+  @Test
+  public void testGetBlockchainParams() {
+    System.out.println("--- testGetBlockchainParams ---");
+
+    CommandManager commandManager =
+        new CommandManager(TestConst.MULTICHAIN_SERVER_IP, TestConst.MULTICHAIN_SERVER_PORT,
+            TestConst.MULTICHAIN_SERVER_LOGIN, TestConst.MULTICHAIN_SERVER_PWD, null);
+
+    HashMap<String, String> result = null;
+
+    try {
+      result = (HashMap<String, String>) commandManager.invoke(CommandElt.GETBLOCKCHAINPARAMS);
+    } catch (MultichainException e) {
+      e.printStackTrace();
+      fail();
+    }
+
+    assertNotNull(result);
+    for (String key : result.keySet()) {
+      System.out.println(key + " - " + String.valueOf(result.get(key)));
+    }
+
+  }
+}
+
---
+++ src/test/java/multichain/object/formatters/GenericOutputFormatterTest.java
@@ -0,0 +1,139 @@
+/*
+ * Copyright (C) 2017 Worldline, Inc.
+ *
+ * MultiChainJavaAPI code distributed under the GPLv3 license, see COPYING file.
+ * https://github.com/SimplyUb/MultiChainJavaAPI/blob/master/LICENSE
+ *
+ */
+package multichain.object.formatters;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Test;
+import com.google.gson.Gson;
+import com.google.gson.internal.LinkedTreeMap;
+import multichain.command.CommandElt;
+
+/**
+ * @author Ub - H. MARTEAU
+ * @version 2.0.1
+ */
+public class GenericOutputFormatterTest {
+
+  @Test
+  public void testFormatBlock() {
+    String blockJsonValue =
+        "{\"hash\": \"00cd11ab4a7146236595c29f52a874521845cb2a417da4e938266e1a3a1f3bb2\","
+            + "\"miner\": \"13SKg7Qw7UxPWvjDMkskyvKU7HfM9mz1KHi43E\"," + "\"confirmations\": 235,"
+            + "\"size\": 241," + "\"height\": 0," + "\"version\": 1,"
+            + "\"merkleroot\": \"428c3d6b97720175dba80c893ec97407522f564af5665ecff0684cd8d74eae6e\","
+            + "\"tx\": [" + "  \"428c3d6b97720175dba80c893ec97407522f564af5665ecff0684cd8d74eae6e\""
+            + "]," + "\"time\": 1543504235," + "\"nonce\": 97," + "\"bits\": \"2000ffff\","
+            + "\"difficulty\": 5.96046447753906E-8,"
+            + "\"chainwork\": \"0000000000000000000000000000000000000000000000000000000000000100\","
+            + "\"nextblockhash\": \"0074f303c0b50dcf4a0f8e48840c14af9dd5666f5f953b154b9d7d2549ea02f9\""
+            + "}";
+
+    Class<?>[] returnedTypes = CommandElt.GETBLOCK.getReturnedClass();
+    LinkedTreeMap<String, Object> linkedTreeMapValue =
+        new Gson().fromJson(blockJsonValue, LinkedTreeMap.class);
+
+    Object returnedValue = GenericOutputFormatter.format(linkedTreeMapValue, returnedTypes);
+
+    System.out.println(returnedValue);
+    System.out.println(returnedValue.getClass());
+
+    Assert.assertNotNull(returnedValue);
+
+  }
+
+  @Test
+  public void testFormatAddressesSimple() {
+    String addresseJsonValue01 = "\"11ADDRESSE01ADDRESSE01ADDRESSE01111111\"";
+    String addresseJsonValue02 = "\"22ADDRESSE02ADDRESSE02ADDRESSE02222222\"";
+    String addresseJsonValue03 = "\"33ADDRESSE03ADDRESSE03ADDRESSE03333333\"";
+    String addresseJsonValue04 = "\"44ADDRESSE04ADDRESSE04ADDRESSE04444444\"";
+
+    Class<?>[] returnedTypes = CommandElt.GETADDRESSES.getReturnedClass();
+    String linkedTreeMapValue1 = new Gson().fromJson(addresseJsonValue01, String.class);
+    String linkedTreeMapValue2 = new Gson().fromJson(addresseJsonValue02, String.class);
+    String linkedTreeMapValue3 = new Gson().fromJson(addresseJsonValue03, String.class);
+    String linkedTreeMapValue4 = new Gson().fromJson(addresseJsonValue04, String.class);
+
+    List<String> listLinkedTreeMap = new ArrayList<>();
+    listLinkedTreeMap.add(linkedTreeMapValue1);
+    listLinkedTreeMap.add(linkedTreeMapValue2);
+    listLinkedTreeMap.add(linkedTreeMapValue3);
+    listLinkedTreeMap.add(linkedTreeMapValue4);
+
+    Object returnedValue = GenericOutputFormatter.formatList(listLinkedTreeMap, returnedTypes);
+
+    System.out.println(returnedValue);
+    System.out.println(returnedValue.getClass());
+    if (ArrayList.class.isInstance(returnedValue) && returnedValue != null
+        && ((ArrayList) returnedValue).size() > 0) {
+      System.out.println(((ArrayList) returnedValue).get(0).getClass());
+    }
+
+    Assert.assertNotNull(returnedValue);
+
+  }
+
+
+
+  @Test
+  public void testFormatAddressesVerbose() {
+    String addresseJsonValue01 =
+        "{\"address\": \"11ADDRESSE01ADDRESSE01ADDRESSE01111111\",\"ismine\": true,"
+            + "\"iswatchonly\": false,\"isscript\": false,"
+            + "\"pubkey\": \"000011ADDRESSE01ADDRESSE01ADDRESSE01111111ADRESSE01ADDRESSE0111111\","
+            + "\"iscompressed\": true,\"account\": \"adresse01\"}";
+    String addresseJsonValue02 =
+        "{\"address\": \"22ADDRESSE02ADDRESSE02ADDRESSE02222222\",\"ismine\": true,"
+            + "\"iswatchonly\": false,\"isscript\": false,"
+            + "\"pubkey\": \"000022ADDRESSE02ADDRESSE02ADDRESSE02222222ADRESSE02ADDRESSE0222222\","
+            + "\"iscompressed\": true,\"account\": \"adresse02\"}";
+    String addresseJsonValue03 =
+        "{\"address\": \"33ADDRESSE03ADDRESSE03ADDRESSE03333333\",\"ismine\": true,"
+            + "\"iswatchonly\": false,\"isscript\": false,"
+            + "\"pubkey\": \"000033ADDRESSE03ADDRESSE03ADDRESSE03333333ADRESSE03ADDRESSE0333333\","
+            + "\"iscompressed\": true,\"account\": \"adresse03\"}";
+    String addresseJsonValue04 =
+        "{\"address\": \"44ADDRESSE04ADDRESSE04ADDRESSE04444444\",\"ismine\": true,"
+            + "\"iswatchonly\": false,\"isscript\": false,"
+            + "\"pubkey\": \"000044ADDRESSE04ADDRESSE04ADDRESSE04444444ADRESSE04ADDRESSE0444444\","
+            + "\"iscompressed\": true,\"account\": \"adresse04\"}";
+
+    Class<?>[] returnedTypes = CommandElt.GETADDRESSES.getReturnedClass();
+    LinkedTreeMap<String, Object> linkedTreeMapValue1 =
+        new Gson().fromJson(addresseJsonValue01, LinkedTreeMap.class);
+    LinkedTreeMap<String, Object> linkedTreeMapValue2 =
+        new Gson().fromJson(addresseJsonValue02, LinkedTreeMap.class);
+    LinkedTreeMap<String, Object> linkedTreeMapValue3 =
+        new Gson().fromJson(addresseJsonValue03, LinkedTreeMap.class);
+    LinkedTreeMap<String, Object> linkedTreeMapValue4 =
+        new Gson().fromJson(addresseJsonValue04, LinkedTreeMap.class);
+
+    List<LinkedTreeMap<String, Object>> listLinkedTreeMap = new ArrayList<>();
+    listLinkedTreeMap.add(linkedTreeMapValue1);
+    listLinkedTreeMap.add(linkedTreeMapValue2);
+    listLinkedTreeMap.add(linkedTreeMapValue3);
+    listLinkedTreeMap.add(linkedTreeMapValue4);
+
+    Object returnedValue = GenericOutputFormatter.formatList(listLinkedTreeMap, returnedTypes);
+
+    System.out.println(returnedValue);
+    System.out.println(returnedValue.getClass());
+    if (ArrayList.class.isInstance(returnedValue) && returnedValue != null
+        && ((ArrayList) returnedValue).size() > 0) {
+      System.out.println(((ArrayList) returnedValue).get(0).getClass());
+    }
+
+    Assert.assertNotNull(returnedValue);
+
+  }
+
+
+
+}
