2d6ff93e0eb60ed0cc16d09e9d88a440c37bb5e9
Nikunj Dhameliya <nikunj.dhameliya@techforceglobal.com>
2020-02-21 11:43:52 +0530

    Null transaction issue fixed for LISTWALLETTRANSACTIONS command

--- src/main/java/multichain/object/Item.java
+++ src/main/java/multichain/object/Item.java
@@ -1,13 +1,11 @@
 /*
  * Copyright (C) 2017 Worldline, Inc.
  *
- * MultiChainJavaAPI code distributed under the GPLv3 license, see COPYING file.
- * https://github.com/SimplyUb/MultiChainJavaAPI/blob/master/LICENSE
+ * MultiChainJavaAPI code distributed under the GPLv3 license, see COPYING file. https://github.com/SimplyUb/MultiChainJavaAPI/blob/master/LICENSE
  *
  */
 package multichain.object;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -15,103 +13,162 @@ import java.util.List;
  * @version 2.0.1
  */
 public class Item {
-  String type = null; // For example "Stream"
-  String name = null; // For example name of the Stream
-  String streamref = null;
-  List<String> publishers; // @ of publishers
-  String key = null; // For example key used to publish in the Stream
-  String data = null; // For example data published in the Stream
 
-  /**
-   * 
-   */
-  public Item() {
-    super();
-    publishers = new ArrayList<String>();
-  }
+	String type = null; // For example "Stream"
+	String name = null; // For example name of the Stream
+	String streamref = null;
+	List<String> publishers; // @ of publishers
+	List<String> keys = null; // For example key used to publish in the Stream
+	Boolean offchain = null; // For example data published in the Stream
+	Boolean available = null;
+	Object data = null;
 
-  /**
-   * @return the type
-   */
-  public String getType() {
-    return type;
-  }
+	/**
+	 * @return the type
+	 */
+	public String getType() {
 
-  /**
-   * @param type the type to set
-   */
-  public void setType(String type) {
-    this.type = type;
-  }
+		return type;
+	}
 
-  /**
-   * @return the name
-   */
-  public String getName() {
-    return name;
-  }
+	/**
+	 * @param type
+	 *            the type to set
+	 */
+	public void setType(final String type) {
 
-  /**
-   * @param name the name to set
-   */
-  public void setName(String name) {
-    this.name = name;
-  }
+		this.type = type;
+	}
 
-  /**
-   * @return the streamref
-   */
-  public String getStreamref() {
-    return streamref;
-  }
+	/**
+	 * @return the name
+	 */
+	public String getName() {
 
-  /**
-   * @param streamref the streamref to set
-   */
-  public void setStreamref(String streamref) {
-    this.streamref = streamref;
-  }
+		return name;
+	}
 
-  /**
-   * @return the publishers
-   */
-  public List<String> getPublishers() {
-    return publishers;
-  }
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(final String name) {
 
-  /**
-   * @param publishers the publishers to set
-   */
-  public void setPublishers(List<String> publishers) {
-    this.publishers = publishers;
-  }
+		this.name = name;
+	}
 
-  /**
-   * @return the key
-   */
-  public String getKey() {
-    return key;
-  }
+	/**
+	 * @return the streamref
+	 */
+	public String getStreamref() {
 
-  /**
-   * @param key the key to set
-   */
-  public void setKey(String key) {
-    this.key = key;
-  }
+		return streamref;
+	}
 
-  /**
-   * @return the data
-   */
-  public String getData() {
-    return data;
-  }
+	/**
+	 * @param streamref
+	 *            the streamref to set
+	 */
+	public void setStreamref(final String streamref) {
 
-  /**
-   * @param data the data to set
-   */
-  public void setData(String data) {
-    this.data = data;
-  }
+		this.streamref = streamref;
+	}
+
+	/**
+	 * @return the publishers
+	 */
+	public List<String> getPublishers() {
+
+		return publishers;
+	}
+
+	/**
+	 * @param publishers
+	 *            the publishers to set
+	 */
+	public void setPublishers(final List<String> publishers) {
+
+		this.publishers = publishers;
+	}
+
+	/**
+	 * @return the keys
+	 */
+	public List<String> getKeys() {
+
+		return keys;
+	}
+
+	/**
+	 * @param keys
+	 *            the keys to set
+	 */
+	public void setKeys(final List<String> keys) {
+
+		this.keys = keys;
+	}
+
+	/**
+	 * @return the offchain
+	 */
+	public Boolean getOffchain() {
+
+		return offchain;
+	}
+
+	/**
+	 * @param offchain
+	 *            the offchain to set
+	 */
+	public void setOffchain(final Boolean offchain) {
+
+		this.offchain = offchain;
+	}
+
+	/**
+	 * @return the available
+	 */
+	public Boolean getAvailable() {
+
+		return available;
+	}
+
+	/**
+	 * @param available
+	 *            the available to set
+	 */
+	public void setAvailable(final Boolean available) {
+
+		this.available = available;
+	}
+
+	/**
+	 * @return the data
+	 */
+	public Object getData() {
+
+		return data;
+	}
+
+	/**
+	 * @param data
+	 *            the data to set
+	 */
+	public void setData(final Object data) {
+
+		this.data = data;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+
+		return String.format("Item [type=%s, name=%s, streamref=%s, publishers=%s, keys=%s, offchain=%s, available=%s, data=%s]", type, name, streamref,
+				publishers, keys, offchain, available, data);
+	}
 
 }
